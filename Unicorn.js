6f9d9f902885ae30f39f4fcaef3af5238bd6443f
Randall Luecke <randyl@rclconcepts.com>
2012-02-15 23:24:33 -0500

    Initial Commit

---
+++ Unicorn.js
@@ -0,0 +1,5 @@
+// via https://twitter.com/kitcambridge/status/169999170332135424
+// WRT: https://twitter.com/me1000/status/169986581187407872
+// WRT: https://twitter.com/schwa/status/169986329071984640
+// lul
+with (Object.prototype) toString = valueOf = hasOwnProperty = isPrototypeOf = propertyIsEnumerable = null;
